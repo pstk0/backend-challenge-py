6d5ef4619ae70cd2da4ba07a1a5e4d3e66757e9a
Pedro <silvestre.pedrol1@gmail.com>
2024-06-25 13:52:28 +0100

    Create models.py
    Create models.py

---
+++ models.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String, Float, Date # type: ignore
+from .database import Base
+
+class CurrencyRate(Base):
+    __tablename__ = "currency_rates"
+
+    id = Column(Integer, primary_key=True, index=True)
+    date = Column(Date, unique=True, index=True)
+    rate = Column(Float, nullable=False)
